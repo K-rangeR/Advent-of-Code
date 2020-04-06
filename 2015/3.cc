c858d5cde66d9ae00a6992c46e3fe7b6b80c1944
Kyle Ranger <zranger2345@gmail.com>
2020-04-06 00:42:55 -0500

    2015 - day 3 - part 2

--- 2015/3.cc
+++ 2015/3.cc
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <string>
 #include <unordered_set>
+#include <vector>
 
 struct Coordinate {
   int x;
@@ -13,8 +14,7 @@ struct Coordinate {
   bool operator==(const Coordinate& c) const
   {
     return (this->x == c.x) && (this->y == c.y);
-  }
-};
+  } };
 
 class CoordinateHashFunc {
 public:
@@ -24,7 +24,24 @@ public:
   }
 };
 
-int main()
+void update_coord(const char dir, Coordinate& coord, const Coordinate& prev)
+{
+  if (dir == '^') {
+    coord.y = prev.y + 1; 
+    coord.x = prev.x;
+  } else if (dir == 'v') {
+    coord.y = prev.y - 1; 
+    coord.x = prev.x;
+  } else if (dir == '>') {
+    coord.x = prev.x + 1; 
+    coord.y = prev.y;
+  } else {
+    coord.x = prev.x - 1; 
+    coord.y = prev.y;
+  }
+}
+
+void part_one()
 {
   std::ifstream input("3_input.txt");
   std::string line;
@@ -36,22 +53,48 @@ int main()
   coords.insert(curr_coord);
   for (auto c : line) {
     Coordinate coord(0,0);
-    if (c == '^') {         // north
-      coord.y = curr_coord.y + 1; 
-      coord.x = curr_coord.x;
-    } else if (c == 'v') {  // south
-      coord.y = curr_coord.y - 1; 
-      coord.x = curr_coord.x;
-    } else if (c == '>') {  // east
-      coord.x = curr_coord.x + 1; 
-      coord.y = curr_coord.y;
-    } else {                // west
-      coord.x = curr_coord.x - 1; 
-      coord.y = curr_coord.y;
-    }
+    update_coord(c, coord, curr_coord);
     coords.insert(coord);
     curr_coord = coord;
   }
   std::cout << coords.size() << "\n";
+}
+
+void part_two()
+{ 
+  std::ifstream input("3_input.txt");
+  std::string line;
+  input >> line;
+  input.close();
+
+  std::vector<Coordinate> santa{};
+  santa.push_back(Coordinate(0, 0));
+
+  std::vector<Coordinate> robo{};
+  robo.push_back(Coordinate(0, 0));
+
+  int dir_count = 1;
+  for (auto d : line) {
+    Coordinate coord(0, 0);
+    if (dir_count % 2 == 1) {
+      update_coord(d, coord, santa[santa.size()-1]);
+      santa.push_back(coord);
+    } else {
+      update_coord(d, coord, robo[robo.size()-1]);
+      robo.push_back(coord);
+    }
+    dir_count += 1;
+  }
+
+  std::vector<Coordinate> sr = santa;
+  sr.insert(sr.end(), robo.begin(), robo.end());
+  std::unordered_set<Coordinate, CoordinateHashFunc> coords(sr.begin(), sr.end());
+  std::cout << coords.size() << "\n";
+}
+
+int main()
+{
+  part_one();
+  part_two();
   return 0; 
 }
