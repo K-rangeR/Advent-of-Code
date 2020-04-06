b6ef549e1af5daf83979f6e9d5dadb861c4efefa
Kyle Ranger <zranger2345@gmail.com>
2020-04-06 01:01:15 -0500

    2015 - day 4 - part 1

---
+++ 2015/4.py
@@ -0,0 +1,13 @@
+import hashlib
+import sys
+
+key = "yzbqklnj"
+
+for i in range(sys.maxsize):
+  data = (key + str(i)).encode('utf-8')
+  m = hashlib.md5()
+  m.update(data)
+  hash_val = m.hexdigest()
+  if hash_val[:5] == '00000':
+    print(i)
+    sys.exit(0)
