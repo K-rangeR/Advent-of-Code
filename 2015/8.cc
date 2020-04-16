d92cdc0d766db9fb5fdedf4954b201701600218d
Kyle Ranger <zranger2345@gmail.com>
2020-04-15 20:47:35 -0500

    2015 - day 8 - part 2

--- 2015/8.cc
+++ 2015/8.cc
@@ -4,14 +4,41 @@
 #include <string>
 #include <iterator>
 
-int main()
+int get_real_len(const std::string& str)
 {
-  std::ifstream input("8_input.txt");
-  if (!input) {
-    std::cout << "Could not open input file\n";
-    return 1;
+  int len = 0;
+  for (auto it = str.begin(); it < str.end(); it++) {
+    if (*it == '\"') {
+      std::cout << "found quote: " << *it << "\n";
+      len += 2;
+    } else if (*it == '\\') {
+      std::cout << "found slash: " << *it << "\n";
+      len += 2;
+    } else {
+      len += 1;
+    }
+  }
+  return len + 2; // add in the surrounding double quotes
+}
+
+void part_2(std::ifstream& input)
+{
+  int total_code_len = 0, real_string_len = 0;
+  std::string line;
+  while (std::getline(input, line)) {
+    total_code_len += line.length();
+    real_string_len += get_real_len(line);
   }
 
+  std::cout << "Code length: " << total_code_len << "\n";
+  std::cout << "Real string length: " << real_string_len << "\n";
+
+  int answer = real_string_len - total_code_len;
+  std::cout << "[2] Answer: " << answer << "\n";
+}
+
+void part_1(std::ifstream& input)
+{
   std::regex e("\\\\(x(\\d|[a-f])(\\d|[a-f])|\\\\|\")");
 
   int total_code_len = 0, total_str_len = 0;
@@ -23,7 +50,6 @@ int main()
                        line.begin()+1, 
                        line.end()-1,
                        e, "A");
-    //std::cout << res << "\n";
     total_str_len += res.length();
   }
 
@@ -31,7 +57,19 @@ int main()
   std::cout << "String length: " << total_str_len << "\n";
 
   int answer = total_code_len - total_str_len;
-  std::cout << "Answer: " << answer << "\n";
+  std::cout << "[1] Answer: " << answer << "\n";
+}
+
+int main()
+{
+  std::ifstream input("8_input.txt");
+  if (!input) {
+    std::cout << "Could not open input file\n";
+    return 1;
+  }
+
+  part_1(input);
+  part_2(input);
 
   input.close();
   return 0;
