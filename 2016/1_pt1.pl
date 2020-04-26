ed75a228793180dd6d0547c9536cc4b2b992bc6b
Kyle Ranger <zranger2345@gmail.com>
2020-04-26 00:17:21 -0500

    2016 - day 1 - part 1

---
+++ 2016/1_pt1.pl
@@ -0,0 +1,45 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+open(my $input_file, "<", "1_input.txt") or die "Can't open input file\n";
+
+my %start = (x => 0, y => 0);
+my %end = (x => 0, y => 0);
+my $direction_facing = "N";
+
+chomp(my $input = <$input_file>);
+my @directions = split(/, /, $input);
+foreach (@directions) {
+	my ($direction, $distance) = $_ =~ /([RL]{1})(\d+)/;
+	if ($direction_facing eq "N" and $direction eq "R") {
+		$end{"x"} += $distance;	
+		$direction_facing = "E";
+	} elsif ($direction_facing eq "N" and $direction eq "L") {
+		$end{"x"} -= $distance;	
+		$direction_facing = "W";
+	} elsif ($direction_facing eq "S" and $direction eq "R") {
+		$end{"x"} -= $distance;	
+		$direction_facing = "W";
+	} elsif ($direction_facing eq "S" and $direction eq "L") {
+		$end{"x"} += $distance;	
+		$direction_facing = "E";
+	} elsif ($direction_facing eq "E" and $direction eq "R") {
+		$end{"y"} -= $distance;
+		$direction_facing = "S";
+	} elsif ($direction_facing eq "E" and $direction eq "L") {
+		$end{"y"} += $distance;
+		$direction_facing = "N";
+	} elsif ($direction_facing eq "W" and $direction eq "R") {
+		$end{"y"} += $distance;
+		$direction_facing = "N";
+	} elsif ($direction_facing eq "W" and $direction eq "L") {
+		$end{"y"} -= $distance;
+		$direction_facing = "S";
+	}
+}
+
+my $answer = abs($start{"x"} - $end{"x"}) + abs($start{"y"} - $end{"y"});
+print "Answer = $answer\n";
+
+close $input_file;
