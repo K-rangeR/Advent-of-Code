2f239961cb4cf9987b7741972e38ba8c299b8b3f
Kyle Ranger <zranger2345@gmail.com>
2020-05-04 16:29:54 -0500

    2015 - day 17 - part 2

--- 2015/17.cc
+++ 2015/17.cc
@@ -2,7 +2,11 @@
 #include <vector>
 #include <fstream>
 
-int get_answer(int at, int liters_left, const std::vector<int>& data)
+int get_answer(int at, 
+               int liters_left, 
+               int containers,
+               const std::vector<int>& data,
+               std::vector<int>& container_count)
 {
   if (at == data.size()) {
     return 0;
@@ -10,12 +14,15 @@ int get_answer(int at, int liters_left, const std::vector<int>& data)
 
   int curr_count = 0;
   if (data[at] == liters_left) {
+    container_count.push_back(containers+1);
     curr_count = 1;
   } else if (data[at] < liters_left) {
-    curr_count = get_answer(at+1, liters_left - data[at], data);
+    curr_count = get_answer(at+1, liters_left-data[at], containers+1, 
+                            data, container_count);
   }
 
-  return curr_count + get_answer(at+1, liters_left, data);
+  return curr_count + get_answer(at+1, liters_left, containers, 
+                                 data, container_count);
 }
 
 int main()
@@ -32,8 +39,18 @@ int main()
     data.push_back(std::stoi(line));
   }
 
-  int answer = get_answer(0, 150, data);
-  std::cout << "Answer: " << answer << "\n";
+  std::vector<int> container_count {};
+  int answer = get_answer(0, 150, 0, data, container_count);
+  std::cout << "Part #1 answer: " << answer << "\n";
+
+  auto min = std::min_element(container_count.begin(), container_count.end());
+  int answer2 = 0;
+  for (auto i : container_count) {
+    if (i == *min) {
+      answer2++;
+    }
+  }
+  std::cout << "Part #2 answer: " << answer2 << "\n";
 
   input.close();
   return 0;
