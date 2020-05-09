df59de63461340f9822cdc7ed409b2e5eed8d258
Kyle Ranger <zranger2345@gmail.com>
2020-05-09 18:14:51 -0500

    2016 - day 5 - part 2

--- 2016/05.pl
+++ 2016/05.pl
@@ -6,15 +6,25 @@ use Digest::MD5 qw(md5 md5_hex);
 my $id = "cxdnnyjw";
 my $i = 0;
 my $count = 0;
-my $pwd = "";
+my @pwd = ("", "", "", "", "", "", "", "");
+my @invalid = ("8", "9", "a", "b", "c", "d", "e", "f");
 
 while ($count < 8) {
   my $digest = md5_hex($id . $i);
   if (substr($digest, 0, 5) eq "00000") {
-    $pwd .= substr($digest, 5, 1);
-    $count++;
+    my $pos = substr($digest, 5, 1);
+    my $char = substr($digest, 6, 1);
+    if (grep(/^$pos$/, @invalid)) {
+      $i++;
+      next;
+    }
+    if ($pwd[$pos] eq "") {
+      $pwd[$pos] = $char;
+      $count++;
+    }
   }
   $i++;
 }
 
-print "Password: $pwd\n";
+my $answer = join "", @pwd;
+print "Password: $answer\n";
