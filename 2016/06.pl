6a95ab0598bc94838d91c8ee5c1fb7c5bda7248d
Kyle Ranger <zranger2345@gmail.com>
2020-05-11 21:31:40 -0500

    2016 - day 6 - part 1

---
+++ 2016/06.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+open my $input, "<", "06_input.txt" or die "Can't open input file";
+
+my @char_count = ();
+while (my $line = <$input>) {
+  chomp $line;
+  for (my $i = 0; $i < length($line); $i++) {
+    my $char = substr $line, $i, 1;    
+    $char_count[$i]{$char} += 1
+  }
+}
+
+my $answer = "";
+foreach my $hash (@char_count) {
+  my %hash = %$hash;
+  my $most_frequent = (sort {$hash{$b} <=> $hash{$a}} keys %hash)[0];
+  $answer .= $most_frequent;
+}
+
+print "Answer: $answer\n";
+close $input;
