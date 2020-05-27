164d77e6d21190a0045b34cf9960016e2eda1abd
Kyle Ranger <zranger2345@gmail.com>
2020-05-27 11:35:29 -0500

    2015 - day 20 - part 2

--- 2015/20.cc
+++ 2015/20.cc
@@ -3,15 +3,21 @@
 #include <numeric>
 #include <math.h>
 
+constexpr int input = 33100000;
+
 std::vector<double> get_factors(int n)
 {
   std::vector<double> factors {};
   double sqrt_n = sqrt(n);
-  for (double i = 1.0; i < sqrt_n; ++i) {
+  for (double i = 1.0; i <= sqrt_n; ++i) {
     if (n % (int)i == 0) {
-      factors.push_back(i);      
+      if (i <= 50) {
+        factors.push_back(i);
+      }
       if (i != sqrt_n) {
-        factors.push_back((n / i));
+        if (i <= 50) {
+          factors.push_back((n / i));
+        }
       }
     }
   }
@@ -20,18 +26,17 @@ std::vector<double> get_factors(int n)
 
 int main()
 {
-  constexpr int input = 33100000;
   for (int house = 1; house < std::numeric_limits<int>::max(); house++) {
     auto factors = get_factors(house);
     int total = 0;
     for (auto factor : factors) {
-      total += (factor * 10); 
+      total += (factor * 11);  // 10 for part 2
     }
     if (total >= input) {
       std::cout << "Answer: " << house << "\n";
       break;
     }
   }
-  
+
   return 0;
 }
