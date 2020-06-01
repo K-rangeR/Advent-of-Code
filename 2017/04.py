14ca35be2f018c7dadeb48f8d57cff50809789c9
Kyle Ranger <zranger2345@gmail.com>
2020-05-31 20:27:00 -0500

    2017 - day 4 - part 2

--- 2017/04.py
+++ 2017/04.py
@@ -1,8 +1,21 @@
+#!/usr/bin/env python3
+
+def contains_anagram(words):
+  for (i, word) in enumerate(words):
+    for (j, word2) in enumerate(words):
+      if (i == j) or (len(word) != len(word2)):
+        continue
+      word_sorted = ''.join(sorted(word))
+      word2_sorted = ''.join(sorted(word2))
+      if word_sorted == word2_sorted:
+        return True
+  return False
+
+
 answer = 0
 with open('04_input.txt', 'r') as f:
   for passphrase in f:
     passphrase = passphrase.strip().split()
-    if len(passphrase) == len(set(passphrase)):
-      print(passphrase)
+    if not contains_anagram(passphrase):
       answer += 1
 print("Answer:", answer)
