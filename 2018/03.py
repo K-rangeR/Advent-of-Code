5f9d2c13c7ca2199259df120e2415584dbb65059
Kyle Ranger <zranger2345@gmail.com>
2020-06-03 21:58:37 -0500

    2018 - day 3 - part 2

--- 2018/03.py
+++ 2018/03.py
@@ -2,11 +2,11 @@
 from collections import defaultdict
 
 def parse_line(line):
-  args = line.strip().split()
+  args = line.split()
   x, y = args[2].split(',')
   y = y[:-1]
   width, height = args[3].split('x')
-  return (int(x), int(y), int(width), int(height))
+  return (int(args[0][1:]), int(x), int(y), int(width), int(height))
 
 
 def update_cells(x, y, width, height, cells):
@@ -16,13 +16,28 @@ def update_cells(x, y, width, height, cells):
 
 
 cells = defaultdict(lambda: 0, defaultdict(int))
-with open('03_input.txt', 'r') as data:
-  for line in data:
-    x, y, width, height = parse_line(line)
-    update_cells(x, y, width, height, cells)
+data = open('03_input.txt', 'r').read().strip().split('\n')
+
+# Part 1
+for line in data:
+  _, x, y, width, height = parse_line(line)
+  update_cells(x, y, width, height, cells)
 
 answer = 0
 for count in cells.values():
   if count >= 2:
     answer += 1
-print('Answer:', answer)
+print('Part 1 answer:', answer)
+
+
+# Part 2
+for line in data:
+  claim_id, x, y, width, height = parse_line(line)
+  found = True
+  for i in range(width):
+    for j in range(height):
+      if cells[(x+i, y+j)] > 1:
+        found = False
+  if found:
+    print('Part 2 answer:', claim_id)
+    break
