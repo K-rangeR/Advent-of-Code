a12b4f3f7647d2898d3c4ebb367505818ae20c6a
Kyle Ranger <zranger2345@gmail.com>
2020-06-06 16:18:42 -0500

    2018 - day 4 - part 2

--- 2018/04.py
+++ 2018/04.py
@@ -28,21 +28,26 @@ class Guard:
     return wake_up - sleep
 
   def get_most_common_slept_minute(self):
+    if len(self.sleep_intervals) == 0:
+      return (-1, -1)
+
     tbl = defaultdict(lambda: 0, defaultdict(int))
     for (sleep, wake) in self.sleep_intervals:
       for i in range(sleep, wake+1):
         tbl[i] += 1
-    return max(tbl.items(), key=operator.itemgetter(1))[0]
+    return max(tbl.items(), key=operator.itemgetter(1))
 
 
 pattern = re.compile(r'\[(\d{4}\-\d{2}\-\d{2}) (\d{2}:\d{2})\].*')
 get_time = re.compile(r'\d{2}:(\d{2})')
 
+
 def main():
   timestamps = open('04_input.txt', 'r').read().strip().split('\n')
   timestamps.sort(key=sort_by_date_then_by_time)
 
-  part_1(timestamps)
+  #part_1(timestamps)
+  part_2(timestamps)
 
   
 def sort_by_date_then_by_time(timestamp):
@@ -51,6 +56,34 @@ def sort_by_date_then_by_time(timestamp):
 
 
 def part_1(timestamps):
+  guards = process_input(timestamps)
+  sleepiest_guard = None
+  max_sleep = -1
+  for (ID, guard) in guards.items():
+    total_sleep_time = guard.get_total_sleep_time()
+    if total_sleep_time > max_sleep:
+      max_sleep = total_sleep_time
+      sleepiest_guard = guard
+
+  answer = sleepiest_guard.get_most_common_slept_minute()[0] * sleepiest_guard.id
+  print('Answer:', answer)
+
+
+def part_2(timestamps):
+  guards = process_input(timestamps)
+  guard_id = -1
+  minute_count = -1
+  minute = -1
+  for guard in guards.values():
+    most_freq_min, count = guard.get_most_common_slept_minute()
+    if count > minute_count:
+      minute_count = count
+      guard_id = guard.id
+      minute = most_freq_min
+  print('Answer:', (guard_id * minute))
+
+
+def process_input(timestamps):
   guards = {}
   curr_guard_id = -1
   for timestamp in timestamps:
@@ -72,17 +105,7 @@ def part_1(timestamps):
     else:
       print('Unknown token to:', tokens[2])
       sys.exit(1)
-
-  sleepiest_guard = None
-  max_sleep = -1
-  for (ID, guard) in guards.items():
-    total_sleep_time = guard.get_total_sleep_time()
-    if total_sleep_time > max_sleep:
-      max_sleep = total_sleep_time
-      sleepiest_guard = guard
-
-  answer = sleepiest_guard.get_most_common_slept_minute() * sleepiest_guard.id
-  print('Answer:', answer)
+  return guards
 
 
 if __name__ == '__main__':
