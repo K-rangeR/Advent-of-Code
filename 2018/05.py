37f9c6ef4b894762247ecfa21a2f4a4266c65a1c
Kyle Ranger <zranger2345@gmail.com>
2020-06-07 13:43:29 -0500

    2018 - day 5 - part 2

--- 2018/05.py
+++ 2018/05.py
@@ -1,8 +1,23 @@
 #!/usr/bin/env python3
+import math
+from string import ascii_lowercase
 
 def main():
   polymer = list(open('05_input.txt', 'r').read().strip())
+  print('Answer part 1:', reacted_len(polymer))
+  part_two(polymer)
 
+
+def part_two(polymer):
+  min_len = math.inf
+  for c in ascii_lowercase:
+    tmp_polymer = [unit for unit in polymer]
+    tmp_polymer[:] = [unit for unit in tmp_polymer if unit != c and unit != c.upper()]
+    min_len = min(min_len, reacted_len(tmp_polymer))
+  print('Answer part 2:', min_len)
+
+
+def reacted_len(polymer):
   stack = [polymer[0]] 
   for unit in polymer[1:]:
     if len(stack) == 0:
@@ -13,8 +28,7 @@ def main():
       stack.pop()
     else:
       stack.append(unit)
-
-  print('Answer:', len(stack))
+  return len(stack)
 
   
 def same_but_different_case(a, b):
