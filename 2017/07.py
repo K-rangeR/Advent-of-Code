a0f459b47c835beb613ebe13d71fef76100c02de
Kyle Ranger <zranger2345@gmail.com>
2020-06-09 12:31:53 -0500

    2017 - day 7 - refactor

--- 2017/07.py
+++ 2017/07.py
@@ -1,20 +1,35 @@
 #!/usr/bin/env python3
-graph = {}
+
+class Node:
+  def __init__(self, name, weight, neighbors):
+    self.name = name
+    self.weight = weight
+    if len(neighbors):
+      self.neighbors = {neighbor: None for neighbor in neighbors}
+    else:
+      self.neighbors = {}
+
+
+graph = []
 with open('07_input.txt', 'r') as input_data:
   for line in input_data:
     line = line.strip().split(' ')
+    name = line[0]
+    weight = int(line[1][1:-1])
     if len(line) > 2:
       edges = [tower.strip(',') for tower in line[3:]]
-      graph[line[0]] = {edge: None for (i, edge) in enumerate(edges)}
+      graph.append(Node(name, weight, edges))
     else:
-      graph[line[0]] = {}
+      graph.append(Node(name, weight, []))
 
-for node in graph.keys():
+
+# Part 1
+for node in graph:
   found = False
-  for node2 in graph.keys():
-    if node in graph[node2]:
+  for node2 in graph:
+    if node.name in node2.neighbors:
       found = True
       break
   if not found:
-    print('Answer:', node)
+    print('Answer:', node.name)
     break
