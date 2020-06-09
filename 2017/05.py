69b537527f04e42a33b33be103b684f6d5698d7d
Kyle Ranger <zranger2345@gmail.com>
2020-06-09 10:58:05 -0500

    2017 - day 5 - part 1

---
+++ 2017/05.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+data = open('05_input.txt', 'r').read().strip().split('\n')
+data = [int(i) for i in data]
+
+offset, steps = 0, 0
+while offset < len(data):
+  jump = data[offset]
+  data[offset] += 1
+  offset += jump
+  steps += 1
+
+print('Answer:', steps)
