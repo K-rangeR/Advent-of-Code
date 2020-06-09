cbc3e02e9696e0a90f8a3e36fcfa1d66acd75d67
Kyle Ranger <zranger2345@gmail.com>
2020-06-09 11:44:41 -0500

    2017 - day 6 - part 1

---
+++ 2017/06.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+def get_max_bank_index(banks):
+  max_blocks, max_blocks_index = banks[0], 0
+  for (i,blocks) in enumerate(banks[1:]):
+    if blocks > max_blocks:
+      max_blocks, max_blocks_index = blocks, i+1
+  return max_blocks_index
+
+
+def redistribute_blocks(banks, from_block):
+  blocks, i = banks[from_block], (from_block+1 % len(banks))
+  banks[from_block] = 0
+  while blocks >= 1:
+    banks[(i % len(banks))] += 1
+    blocks -= 1
+    i += 1
+
+
+banks = open('06_input.txt', 'r').read().strip().split('\t')
+banks = [int(i) for i in banks]
+mem_states = {tuple(banks):True}
+cycles = 0
+
+while True:
+  cycles += 1
+  index = get_max_bank_index(banks)
+  redistribute_blocks(banks, index)
+  if tuple(banks) in mem_states:
+    break
+  else:
+    mem_states[tuple(banks)] = True
+
+print('Answer:', cycles)
