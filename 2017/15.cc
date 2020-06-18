5afefd0101008f19da6c712c6375eafe35aad28f
Kyle Ranger <zranger2345@gmail.com>
2020-06-18 14:07:24 -0500

    Use uint64_t instead of unsigned long

--- 2017/15.cc
+++ 2017/15.cc
@@ -2,17 +2,17 @@
 
 int main()
 {
-  constexpr unsigned long gen_a_factor = 16807;
-  constexpr unsigned long gen_b_factor = 48271;
-  constexpr unsigned long div_by = 2147483647;
-  unsigned long gen_a_prev = 277, gen_b_prev = 349;
-  constexpr unsigned long mask = (1 << 16) - 1;
-  constexpr unsigned long to = 40000000;
+  constexpr uint64_t gen_a_factor = 16807;
+  constexpr uint64_t gen_b_factor = 48271;
+  constexpr uint64_t div_by = 2147483647;
+  uint64_t gen_a_prev = 277, gen_b_prev = 349;
+  constexpr uint64_t mask = (1 << 16) - 1;
+  constexpr uint64_t to = 40000000;
   int answer = 0;
 
   for (int i = 0; i < to; ++i) {
-    unsigned long next_a = (gen_a_prev * gen_a_factor) % div_by;
-    unsigned long next_b = (gen_b_prev * gen_b_factor) % div_by;
+    uint64_t next_a = (gen_a_prev * gen_a_factor) % div_by;
+    uint64_t next_b = (gen_b_prev * gen_b_factor) % div_by;
     if ((next_a & mask) == (next_b & mask)) {
       answer++;
     }
