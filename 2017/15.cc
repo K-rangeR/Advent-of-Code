fbe9a239669a410f4adab65955a2d18f2488a6c0
Kyle Ranger <zranger2345@gmail.com>
2020-06-20 14:15:04 -0500

    2017 - day 15 - part 2

--- 2017/15.cc
+++ 2017/15.cc
@@ -7,19 +7,26 @@ int main()
   constexpr uint64_t div_by = 2147483647;
   uint64_t gen_a_prev = 277, gen_b_prev = 349;
   constexpr uint64_t mask = (1 << 16) - 1;
-  constexpr uint64_t to = 40000000;
+  constexpr uint64_t to = 5000000;
+  uint64_t next_a = 0, next_b = 0;
   int answer = 0;
 
   for (int i = 0; i < to; ++i) {
-    uint64_t next_a = (gen_a_prev * gen_a_factor) % div_by;
-    uint64_t next_b = (gen_b_prev * gen_b_factor) % div_by;
+    do {
+      next_a = (gen_a_prev * gen_a_factor) % div_by;
+      gen_a_prev = next_a;
+    } while (next_a % 4);
+
+    do {
+      next_b = (gen_b_prev * gen_b_factor) % div_by;
+      gen_b_prev = next_b;
+    } while (next_b % 8);
+
     if ((next_a & mask) == (next_b & mask)) {
       answer++;
     }
-    gen_a_prev = next_a;
-    gen_b_prev = next_b;
   }
 
-  std::cout << "Part #1: " << answer << "\n";
+  std::cout << answer << "\n";
   return 0;
 }
