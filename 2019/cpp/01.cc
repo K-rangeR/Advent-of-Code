ef5aac017a3f4b96738796823daeaa1bd22c767a
Kyle Ranger <zranger2345@gmail.com>
2020-07-02 17:15:57 -0500

    2019 - day 1 - part 1 - c++

--- 2019/cpp/01.cc
+++ 2019/cpp/01.cc
@@ -1,6 +1,26 @@
 #include <iostream>
+#include <fstream>
+
+inline int fuel(int mass)
+{
+  return (mass / 3) - 2;
+}
 
 int main()
 {
+  std::ifstream input("01_input.txt"); 
+  if (!input) {
+    std::cerr << "Could not open input file\n";
+    return 0;
+  }
+
+  int total_fuel = 0;
+  std::string line;
+  while (std::getline(input, line)) {
+    total_fuel += fuel(atoi(line.c_str()));
+  }
+
+  std::cout << "Part #1: " << total_fuel << "\n";
+  input.close();
   return 0;
 }
