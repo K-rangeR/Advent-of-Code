3f8365ced1da8c664d79abea8edbde7101c24a02
Kyle Ranger <zranger2345@gmail.com>
2020-12-01 10:53:05 -0600

    2020 - day 1 - part 2

--- 2020/01_day.py
+++ 2020/01_day.py
@@ -9,12 +9,25 @@ def main():
     p1_answer = part_1(expense_report)
     print(p1_answer)
 
+    p2_answer = part_2(expense_report)
+    print(p2_answer)
 
+# Just brute force it
 def part_1(report):
   for (i,expense1) in enumerate(report):
     for (j,expense2) in enumerate(report):
       if i != j and (expense1 + expense2 == 2020):
         return expense1 * expense2
+  return -1
+
+
+# Just brute force it again!
+def part_2(report):
+  for (i,expense1) in enumerate(report):
+    for (j,expense2) in enumerate(report):
+      for (k,expense3) in enumerate(report):
+        if (i != j and i != k) and (expense1 + expense2 + expense3 == 2020):
+          return expense1 * expense2 * expense3
   return -1
 
 
