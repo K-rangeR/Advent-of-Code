a9ff1dce1b18a8103a2b7218c302562590ee5afb
Kyle Ranger <zranger2345@gmail.com>
2020-12-02 12:06:44 -0600

    2020 - day 2 - part 2

--- 2020/02_day.py
+++ 2020/02_day.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 
 def main():
+  #part1()
+  part2()
+
+
+def part1():
   answer = 0
   with open("./02_input.txt", "r") as f:
     for line in f:
@@ -15,7 +20,23 @@ def main():
       if int(nrange[0]) <= count <= int(nrange[1]):
         answer += 1
     print(answer)
-        
+
+
+def part2():
+  answer = 0
+  with open("./02_input.txt", "r") as f:
+    for line in f:
+      tokens = line.strip().split(" ")
+      nrange = tokens[0].split("-")
+      letter = tokens[1][:1]
+      pwd = "#" + tokens[2]
+      p1, p2 = pwd[int(nrange[0])], pwd[int(nrange[1])]
+      if p1 == p2:
+        continue
+      elif p1 == letter or p2 == letter:
+        answer += 1
+    print(answer)
+  
 
 if __name__ == "__main__":
   main()
