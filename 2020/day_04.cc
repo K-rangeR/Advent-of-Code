2149fb5042745e0124f058f6fd2ad66c6f83b4b9
Kyle Ranger <zranger2345@gmail.com>
2020-12-04 13:50:09 -0600

    2020 - day 4 - part 2

--- 2020/day_04.cc
+++ 2020/day_04.cc
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <fstream>
 #include <unordered_map>
+#include <string>
+#include <regex>
 
-bool is_valid(std::unordered_map<std::string,bool>& fields)
+bool is_document_valid(std::unordered_map<std::string,bool>& fields)
 {
   bool valid = true;
   for (auto key : fields) {
@@ -14,18 +16,64 @@ bool is_valid(std::unordered_map<std::string,bool>& fields)
   return valid;
 }
 
+bool is_kv_pair_valid(const std::string& key, const std::string& value)
+{
+  if (key == "byr") {
+    int byr = std::stoi(value);
+    return byr >= 1920 && byr <= 2002;
+  } else if (key == "iyr") {
+    int iyr = std::stoi(value);
+    return iyr >= 2010 && iyr <= 2020;
+  } else if (key == "eyr") {
+    int eyr = std::stoi(value);
+    return eyr >= 2020 && eyr <= 2030;
+  } else if (key == "hgt") {
+    auto ptr = value.find("c");
+    if (ptr == std::string::npos) {
+      ptr = value.find("i");
+      if (ptr == std::string::npos) {
+        return false;
+      }
+    }
+    int n = std::stoi(value.substr(0, ptr));
+    if (value.at(ptr) == 'c') {
+      return n >= 150 && n <= 193;
+    } else {
+      return n >= 59 && n <= 76;
+    }
+  } else if (key == "hcl") {
+    return std::regex_match(value, std::regex("#[0-9a-f]{6}"));
+  } else if (key == "ecl") {
+    return value == "blu" || value == "gry" || value == "brn" ||
+           value == "grn" || value == "hzl" || value == "oth" || value == "amb";
+  } else if (key == "pid") {
+    return std::regex_match(value, std::regex("[0-9]{9}"));
+  } else if (key == "cid") {
+    return true;
+  }
+
+  return false;
+}
+
 void parse(const std::string& line, 
            std::unordered_map<std::string,bool>& fields)
 {
   int i = 0;
   auto ptr = line.find(":");
+  std::string key, value;
   while (ptr != std::string::npos) {
-    fields[line.substr(i, (ptr - i))] = true;
+    key = line.substr(i, (ptr - i));
 
+    i = ptr+1;
     ptr = line.find(" ", ptr);
-    if (ptr == std::string::npos) {
-      break;
+    if (ptr != std::string::npos) {
+      value = line.substr(i, (ptr - i));
+    } else { // last key value pair
+      value = line.substr(i, std::string::npos);
     }
+
+    fields[key] = is_kv_pair_valid(key, value);
+
     i = ptr+1;
     ptr = line.find(":", ptr);
   }
@@ -49,12 +97,12 @@ int main()
   std::string line;
   while (std::getline(input, line)) {
     if (line.length() == 0) {
-      valid_pwds += is_valid(fields) ? 1 : 0;
+      valid_pwds += is_document_valid(fields) ? 1 : 0;
     } else {
       parse(line, fields);
     }
   }
-  valid_pwds += is_valid(fields) ? 1 : 0; // last line
+  valid_pwds += is_document_valid(fields) ? 1 : 0; // last line
 
   std::cout << "Answer: " << valid_pwds << "\n";
 
